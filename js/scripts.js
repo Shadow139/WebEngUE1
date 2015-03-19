9957260ab9b14aa84a85e6784a37a4e905d62252
Wiktor Manczarski <cloud99_9@hotmail.com>
2015-03-19 23:06:18 +0100

    no message

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Hollow on 19.03.2015.
+ */
+
+function checkForm(form) {
+    if (form.Benutzername.value.length < 4 || form.Benutzername.value.length > 8) {
+        alert("Error: Benutzername muss zwischen 4 und 8 Zeichen lang sein!");
+        form.Benutzername.focus();
+        return false;
+    }
+
+    if (form.Passwort.value.length < 4 || form.Passwort.value.length > 8) {
+        alert("Error: Passwort muss zwischen 4 und 8 Zeichen lang sein!");
+        form.Passwort.focus();
+        return false;
+    }
+
+    return true;
+}

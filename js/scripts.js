3508adf7c24b5b0796685bd69e699a15ab556010
Wiktor Manczarski <cloud99_9@hotmail.com>
2015-03-28 00:42:31 +0100

    awfafafafa

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,65 @@
  */
 
 function checkForm(form) {
+    var ret = true;
+
     if (form.Benutzername.value.length < 4 || form.Benutzername.value.length > 8) {
-        alert("Error: Benutzername muss zwischen 4 und 8 Zeichen lang sein!");
+        //alert("Error: Benutzername muss zwischen 4 und 8 Zeichen lang sein!");
         form.Benutzername.focus();
-        return false;
+
+        var para = document.createElement("p");
+        var node = document.createTextNode("Error: Benutzername muss zwischen 4 und 8 Zeichen lang sein!");
+        para.appendChild(node);
+
+        var element = document.getElementById("error-username");
+        element.appendChild(para);
+
+        ret = false;
     }
 
     if (form.Passwort.value.length < 4 || form.Passwort.value.length > 8) {
-        alert("Error: Passwort muss zwischen 4 und 8 Zeichen lang sein!");
+        //alert("Error: Passwort muss zwischen 4 und 8 Zeichen lang sein!");
+
+        var para = document.createElement("p");
+        var node = document.createTextNode("Error: Passwort muss zwischen 4 und 8 Zeichen lang sein!");
+        para.appendChild(node);
+
+        var element = document.getElementById("error-password");
+        element.appendChild(para);
+
         form.Passwort.focus();
-        return false;
+
+        ret = false;
     }
 
-    return true;
+    return ret;
 }
 
-function checkField(input){
+function checkField(input) {
+    var ret = true;
+    if(document.getElementById("error-message") != null) {
+        var tmp = document.getElementById("error-message");
+        tmp.parentNode.removeChild(tmp);
+    }
+
     if (input.value.length < 4 || input.value.length > 8) {
+        //alert("Error: Benutzername muss zwischen 4 und 8 Zeichen lang sein!");
         input.focus();
-        return false;
+
+        var para = document.createElement("p");
+        para.id = "error-message";
+        var node = document.createTextNode("Error: Feld muss zwischen 4 und 8 Zeichen lang sein!");
+        para.appendChild(node);
+
+        //var element = document.getElementById("input_error");
+        element = input.parentNode;
+        element.appendChild(para);
+
+        para.style.color="red";
+
+        ret = false;
     }
-    return true;
+
+    return ret;
 }
+

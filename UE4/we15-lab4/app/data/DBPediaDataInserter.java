f33a68104aaf4555dacccdfd72c1871c74899faa
Felix Ledóchowski <flm@live.at>
2015-05-27 14:27:55 +0200

    Jubisoft

--- UE4/we15-lab4/app/data/DBPediaDataInserter.java
+++ UE4/we15-lab4/app/data/DBPediaDataInserter.java
@@ -1,8 +1,8 @@
 package data;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 
 import play.Logger;
 import models.Answer;
@@ -28,60 +28,81 @@ public class DBPediaDataInserter {
 		if(!DBPediaService.isAvailable())
 			return;  
 		
-		// Resource Tim Burton is available at http://dbpedia.org/resource/Tim_Burton  
+		// Resource Ubisoft is available at http://dbpedia.org/resource/Ubisoft  
 		// Load all statements as we need to get the name later 
-		Resource director = DBPediaService.loadStatements(DBPedia.createResource("Tim_Burton"));     
-		
-		// Resource Johnny Depp is available at http://dbpedia.org/resource/Johnny_Depp  
-		// Load all statements as we need to get the name later 
-		Resource actor = DBPediaService.loadStatements(DBPedia.createResource("Johnny_Depp"));  
-		
-		// retrieve english and german names, might be used for question text  
-		String englishDirectorName = DBPediaService.getResourceName(director, Locale.ENGLISH); 
-		String germanDirectorName = DBPediaService.getResourceName(director, Locale.GERMAN);  
-		String englishActorName = DBPediaService.getResourceName(actor, Locale.ENGLISH); 
-		String germanActorName = DBPediaService.getResourceName(actor, Locale.GERMAN);  
+		Resource publisher = DBPediaService.loadStatements(DBPedia.createResource("Ubisoft"));     
 		
 		// build SPARQL-query 
-		SelectQueryBuilder movieQuery = DBPediaService.createQueryBuilder()  
-				.setLimit(5) // at most five statements  
-				.addWhereClause(RDF.type, DBPediaOWL.Film)  
+		SelectQueryBuilder gameQuery = DBPediaService.createQueryBuilder()  
+				.setLimit(200) // at most five statements  
+				.addWhereClause(RDF.type, DBPediaOWL.VideoGame)  
 				.addPredicateExistsClause(FOAF.name)  
-				.addWhereClause(DBPediaOWL.director, director)  
+				.addWhereClause(DBPediaOWL.publisher, publisher)  
 				.addFilterClause(RDFS.label, Locale.GERMAN)  
-				.addFilterClause(RDFS.label, Locale.ENGLISH);  // retrieve data from dbpedia         
-		Model timBurtonMovies = DBPediaService.loadStatements(movieQuery.toQueryString());  // get english and german movie names, e.g., for right choices 
-		List<String> englishTimBurtonMovieNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) timBurtonMovies, Locale.ENGLISH); 
-		List<String> germanTimBurtonMovieNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) timBurtonMovies, Locale.GERMAN);     // alter query to get movies without tim burton 
+				.addFilterClause(RDFS.label, Locale.ENGLISH);           
+		gameQuery.setLimit(200);
 		
-		movieQuery.removeWhereClause(DBPediaOWL.director, director); 
-		movieQuery.addMinusClause(DBPediaOWL.director, director);  // retrieve data from dbpedia         
+		// retrieve data from dbpedia
+		Model ubisoftGames = DBPediaService.loadStatements(gameQuery.toQueryString());  
 		
-		Model noTimBurtonMovies = DBPediaService.loadStatements(movieQuery.toQueryString());  // get english and german movie names, e.g., for wrong choices 
-		List<String> englishNoTimBurtonMovieNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) noTimBurtonMovies, Locale.ENGLISH); 
-		List<String> germanNoTimBurtonMovieNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) noTimBurtonMovies, Locale.GERMAN); 	
+		// get english and german game names, e.g., for right choices 
+		List<String> englishUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) ubisoftGames, Locale.ENGLISH); 
+		List<String> germanUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) ubisoftGames, Locale.GERMAN);     
 		
-		Logger.info("size of englishNoTimBurtonMovieNames: " + englishNoTimBurtonMovieNames.size());
-		Logger.info("size of germanNoTimBurtonMovieNames: " + germanNoTimBurtonMovieNames.size());
+		// alter query to get games without Ubisoft 
+		gameQuery.removeWhereClause(DBPediaOWL.publisher, publisher); 
+		gameQuery.addMinusClause(DBPediaOWL.publisher, publisher);        
+		gameQuery.setLimit(200);
+		
+		// retrieve data from dbpedia   
+		Model noUbisoftGames = DBPediaService.loadStatements(gameQuery.toQueryString());  
+		// get english and german game names, e.g., for wrong choices 
+		List<String> englishNoUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) noUbisoftGames, Locale.ENGLISH); 
+		List<String> germanNoUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) noUbisoftGames, Locale.GERMAN); 	
+		
+		
+		gameQuery.addWhereClause(DBPediaOWL.publisher, publisher);
+		gameQuery.removeMinusClause(DBPediaOWL.publisher, publisher); 
+		gameQuery.setLimit(200);
+
+		// retrieve data from dbpedia
+		ubisoftGames = DBPediaService.loadStatements(gameQuery.toQueryString());  
+		
+		// get english and german game names, e.g., for right choices 
+		englishUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) ubisoftGames, Locale.ENGLISH); 
+		germanUbisoftGameNames =     DBPediaService.getResourceNames((com.hp.hpl.jena.rdf.model.Model) ubisoftGames, Locale.GERMAN);     
+		
+		
+		Logger.info("size of englishNoUbisoftGameNames: " + englishNoUbisoftGameNames.size());
+		Logger.info("size of germanNoUbisoftGameNames: " + germanNoUbisoftGameNames.size());
 
 		Category category = new Category();
-		category.setNameDE("TIM BURTONS AlICE FMOCVAFLILEM FILME");
-		category.setNameEN("MOVIES");
-		for(int i = 0; i < 5; i++){
+		category.setNameDE("Ubisoft Spiele");
+		category.setNameEN("Ubisoft Games");
+		Random randomno = new Random();
+		for(int i = 1; i < 8; i++){
 			Question question = new Question();
-			question.setTextDE("Ist das ein Tim Burton Film?");
-			question.setTextEN("Is it a Tim Burton Movie?");
-			question.setValue(10);
+			question.setTextDE("Ist das ein Ubisoft Spiel?");
+			question.setTextEN("Is it an Ubisoft Game?");
+			question.setValue(10*i);
 			
-			Answer rightChoice = new Answer();
-			rightChoice.setTextDE(germanTimBurtonMovieNames.get(i));
-			rightChoice.setTextEN(englishTimBurtonMovieNames.get(i));
-			question.addRightAnswer(rightChoice);
+			int temp = randomno.nextInt(3)+2;
+			Logger.info("gamesize: "+germanUbisoftGameNames.size()+"");
+			Logger.info("gamesizewrong: "+germanNoUbisoftGameNames.size()+"");
+			for(int r = 1; r < temp; r++){
+				int temp2 = randomno.nextInt(1+germanUbisoftGameNames.size()-2);
+				Answer rightChoice = new Answer();
+				rightChoice.setTextDE(germanUbisoftGameNames.get(temp2));
+				rightChoice.setTextEN(englishUbisoftGameNames.get(temp2));
+				question.addRightAnswer(rightChoice);
+			}
+			temp = randomno.nextInt(3)+4;
 			
-			for(int j = 0; j < 3; j++){ // Dieser Teil is noch Buggy wir müssen anschauen wie genau die answers geladen werden
+			for(int j = 1; j < temp; j++){ // Dieser Teil is noch Buggy wir müssen anschauen wie genau die answers geladen werden
 				Answer wrongChoice = new Answer();
-				wrongChoice.setTextDE(englishNoTimBurtonMovieNames.get(j));
-				wrongChoice.setTextEN(germanNoTimBurtonMovieNames.get(j));
+				int temp2 = randomno.nextInt(1+englishNoUbisoftGameNames.size()-2);
+				wrongChoice.setTextDE(englishNoUbisoftGameNames.get(temp2));
+				wrongChoice.setTextEN(germanNoUbisoftGameNames.get(temp2));
 				question.addWrongAnswer(wrongChoice);
 			}
 			
